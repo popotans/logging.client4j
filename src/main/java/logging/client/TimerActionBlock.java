25402e4e9b04e468e2043265b86e82e54a81241e
xumingxiang <xumingxiang@plu.cn>
2015-10-12 21:16:33 +0800

    update

--- src/main/java/logging/client/TimerActionBlock.java
+++ src/main/java/logging/client/TimerActionBlock.java
@@ -3,77 +3,80 @@ package logging.client;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Callable;
 
 public class TimerActionBlock<T> implements ITimerActionBlock<T> {
 
-	LinkedBlockingQueue<T> queue   = new LinkedBlockingQueue<T>(1024);  
-	
-	 /// <summary>
-    /// 阻塞队列的最大长度
-    /// </summary>
-    private int QueueMaxLength;
+	LinkedBlockingQueue<T> queue = new LinkedBlockingQueue<T>(1024);
 
-    /// <summary>
-    /// 元素包的大小
-    /// </summary>
-    private int BufferSize;
+	/// <summary>
+	/// 阻塞队列的最大长度
+	/// </summary>
+	private int QueueMaxLength;
 
-    /// <summary>
-    /// 上一次打包处理的时间
-    /// </summary>
-    private Date LastActionTime;
+	/// <summary>
+	/// 元素包的大小
+	/// </summary>
+	private int BufferSize;
 
-    private int BlockElapsed;
-	
-    private DequeueAction<T> Action;
-    
- 	public	TimerActionBlock(DequeueAction<T> action,int queueMaxLength, int bufferSize, int blockElapsed){
-		this.QueueMaxLength=queueMaxLength;
-		this.BufferSize=bufferSize;
-		this.LastActionTime=new Date();
-		this.BlockElapsed=blockElapsed;
-		this.Action=action;
+	/// <summary>
+	/// 上一次打包处理的时间
+	/// </summary>
+	private Date LastActionTime;
+
+	private ExecutorService pool = Executors.newSingleThreadExecutor();
+
+	private int BlockElapsed;
+
+	private DequeueAction<T> Action;
+
+	public TimerActionBlock(DequeueAction<T> action, int queueMaxLength, int bufferSize, int blockElapsed) {
+		this.QueueMaxLength = queueMaxLength;
+		this.BufferSize = bufferSize;
+		this.LastActionTime = new Date();
+		this.BlockElapsed = blockElapsed;
+		this.Action = action;
 	}
-	
-	public void Enqueue(T item)  {
-		// TODO Auto-generated method stub
-		int queueLen=this.queue.size();
-//		int over_count = 0;
-//		if(queueLen>=this.QueueMaxLength){
-//			  over_count = (queueLen - this.QueueMaxLength) + 1;
-//			  for (int i = 0; i < over_count; i++)
-//              {
-//                  try {
-//					this.queue.take();
-//				} catch (InterruptedException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}//超过队列长度，扔掉
-//              }
-//		}
-		
-		if(queueLen>0&&( queueLen>=BufferSize || (new Date().getTime()-this.LastActionTime.getTime())>=this.BlockElapsed)){
-			
-			List<T> buffer=new ArrayList<T>();
-			for	(int i=0;i<queueLen;i++){
-				
+
+	public void Enqueue(T item) {
+		int queueLen = this.queue.size();
+		if (queueLen > 0 && (queueLen >= BufferSize
+				|| (new Date().getTime() - this.LastActionTime.getTime()) >= this.BlockElapsed)) {
+
+			List<T> buffer = new ArrayList<T>();
+			for (int i = 0; i < queueLen; i++) {
+
 				try {
 					buffer.add(this.queue.take());
+					buffer.add(item);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-			this.Action.execute(buffer);
-			this.LastActionTime=new Date();
+			pool.submit(new DequeueActionTask(buffer));
+			this.LastActionTime = new Date();
+		} else {
+			queue.add(item);
 		}
-		queue.add(item);
 	}
-	
-	public interface DequeueAction<T>
-	{
-		public void execute(List<T> items);		
+
+	private final class DequeueActionTask implements Callable<String> {
+		List<T> _data;
+
+		public DequeueActionTask(List<T> data) {
+			_data = data;
+		}
+
+		public String call() throws Exception {
+			Action.execute(_data);
+			return "ok";
+		}
+	}
+
+	public interface DequeueAction<T> {
+		public void execute(List<T> items);
 	}
 }
